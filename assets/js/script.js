421c7386e151572b427e88eeda5efc48de5bdf20
Kayo Silva <91280312+KayoSilva19@users.noreply.github.com>
2022-04-13 19:00:59 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,33 @@ const menuBtn = document.querySelector('.menu-btn');
 const navMenu = document.querySelector('.nav-menu');
 
 menuBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('mobile-menu');
+  navMenu.classList.toggle('mobile-menu');
 }) 
+
 
 
 window.addEventListener('scroll', () => {
   const navBar = document.querySelector('.nav-bar');
-
+  
   navBar.classList.toggle('active-scroll', window.scrollY > 200)
 })
+
+
+const menuBtnList1 = document.querySelector('.menu-btn-list1');
+const menuBtnList2 = document.querySelector('.menu-btn-list2');
+const menuBtnList3 = document.querySelector('.menu-btn-list3');
+const menuBtnList4 = document.querySelector('.menu-btn-list4');
+
+menuBtnList1.addEventListener('click',() => {
+  navMenu.classList.toggle('mobile-menu');
+})
+menuBtnList2.addEventListener('click',() => {
+  navMenu.classList.toggle('mobile-menu');
+})
+menuBtnList3.addEventListener('click',() => {
+  navMenu.classList.toggle('mobile-menu');
+})
+menuBtnList4.addEventListener('click',() => {
+  navMenu.classList.toggle('mobile-menu');
+})
+
